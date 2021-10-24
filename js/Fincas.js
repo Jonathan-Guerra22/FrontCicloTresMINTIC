f94d828f1db5119436aa9c8952a2aec70d6181f1
Jonathan Guerra <jguerrap1@ucentral.edu.co>
2021-10-23 20:35:38 -0500

    Agregando Funcionalidades completas de Fincas

---
+++ js/Fincas.js
@@ -0,0 +1,163 @@
+function verFincas(){
+    $.ajax({
+       url: "http://localhost:8080/api/Farm/all",
+        type: "GET",
+        dataType: "json",
+        success: function(respuesta){
+            console.log(respuesta);
+            $("#resultadoFincas").empty();
+            RespuestaFincas(respuesta);
+        }
+    });
+}
+function RespuestaFincas(items){
+    let myTable = "<table class='table table-striped table-bordered border-secondary'>";
+
+    if(items.length>0){
+        myTable += "<tr class='table-dark'>";
+        myTable += "<th>ID</th>";
+        myTable += "<th>NAME</th>";
+        myTable += "<th>ADDRESS</th>";
+        myTable += "<th>EXTENSION</th>";
+        myTable += "<th>DESCRIPTION</th>";
+        myTable += "<th>CATEGORY</th>";
+        myTable += "<th>MESSAGES</th>";
+        myTable += "<th>RESERVATIONS</th>";
+        myTable += "<th></th>";
+        myTable += "<th></th>";
+        myTable += "</tr>";
+    }else{
+        myTable += "<th>No hay datos disponibles en la tabla</th>";
+    }
+    
+    for(i=0;i<items.length; i++){
+        let categoria = items[i].category.id+"<br>";
+        categoria += items[i].category.name+"<br>";
+        categoria += items[i].category.description;
+        
+        myTable += "<tr>";
+        myTable += "<td>"+items[i].id+"</td>";
+        myTable += "<td>"+items[i].name+"</td>";
+        myTable += "<td>"+items[i].address+"</td>";
+        myTable += "<td>"+items[i].extension+"</td>";
+        myTable += "<td>"+items[i].description+"</td>";
+        myTable += "<td>"+categoria+"</td>";
+        myTable += "<td>"+items[i].messages+"</td>";
+        myTable += "<td>"+items[i].reservations+"</td>";
+        myTable += "<td><button class='btn btn-warning' onclick='editarF("+items[i].id+")'>Edit</button></td>"
+        myTable += "<td><button class='btn btn-danger' onclick='borrarF("+items[i].id+")'>🗑</button></td>"
+        myTable += "</tr>";
+    }
+    
+    myTable += "</table>";
+    $("#resultadoFincas").append(myTable);
+}
+
+function crearFinca(){
+    
+    let categoria={id: $("#category").val()}
+
+
+    let myData= {
+        address: $("#address").val(),
+        extension: $("#extension").val(),
+        category: categoria,
+        name: $("#name").val(),
+        description: $("#description").val(),
+    };
+    
+    let dataToSend = JSON.stringify(myData);
+
+    $.ajax({
+		url: "http://localhost:8080/api/Farm/save",
+        type:"POST",
+        data: dataToSend,
+        dataType:"json",
+        contentType: 'application/json',
+        complete:function(respuesta){
+
+            $("#address").val("");
+            $("#extension").val("");
+            $("#category").val(""),
+            $("#name").val("");
+            $("#description").val("");
+            
+            verFincas();
+
+            console.log("Guardado!");
+
+        },
+        error: function(textStatus) {
+            console.log(textStatus)
+        }
+    });
+}
+
+function editarF(idF){
+    
+    $.ajax({
+        url: "http://localhost:8080/api/Farm/all",
+         type: "GET",
+         dataType: "json",
+         success: function(respuesta){
+
+            for(i=0;i<respuesta.length; i++){
+                if(respuesta[i].id==idF){
+                    $("#id").empty();
+                    $("#id").val(idF);
+                    $("#addressEditF").empty();
+                    $("#addressEditF").val(respuesta[i].address);
+                    $("#nameEditF").empty();
+                    $("#nameEditF").val(respuesta[i].name);
+                    $("#descriptionEditF").empty();
+                    $("#descriptionEditF").val(respuesta[i].description);
+                    $("#extensionEditF").empty();
+                    $("#extensionEditF").val(respuesta[i].extension);
+                }
+            }
+         }
+     });
+}
+
+function editarFinca(){
+         
+    let myData= {
+        id:$("#id").val(),
+        address: $("#addressEditF").val(),
+        name: $("#nameEditF").val(),
+        description: $("#descriptionEditF").val(),
+        extension: $("#extensionEditF").val(),
+    };
+       
+    let dataToSend = JSON.stringify(myData);
+        
+    $.ajax({
+        url: "http://localhost:8080/api/Farm/update",
+        type:"PUT",
+        data: dataToSend,
+        dataType:"json",
+        contentType: 'application/json',
+        success:function(respuesta){
+            verFincas();
+            $("#id").val("");
+            $("#addressEditF").val("");
+            $("#nameEditF").val("");
+            $("#descriptionEditF").val("");
+            $("#extensionEditF").val("");
+            console.log("Editado");
+        }
+    });
+}
+    
+function borrarF(idF){
+        
+    let urlMV="http://localhost:8080/api/Farm/"+idF;
+    $.ajax({
+        url: urlMV,
+        type:"DELETE",
+        success:function(respuesta){
+            verFincas();
+            console.log("Borrado");
+        }
+    });
+}
